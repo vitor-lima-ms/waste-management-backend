60d5d8567f114234442fd2fbd6ea620464857b05
Vitor Lima <viitorlima2000@gmail.com>
2025-11-13 15:09:53 -0300

    chore: remove useless file

--- src/common/exceptions/bad-request.exception.ts
+++
@@ -1,8 +0,0 @@
-/* Nest.js imports */
-import { HttpException, HttpStatus } from "@nestjs/common";
-/* BadRequestException */
-export class BadRequestException extends HttpException {
-  constructor() {
-    super("Bad Request.", HttpStatus.BAD_REQUEST);
-  }
-}
